42c321cbbc66dc5e36d30dde8227b3b8fd0f1082
Hayo <hayo.ottens@gmail.com>
2019-11-08 12:16:11 +0100

    called by

--- find_setUserAgentString.py
+++ find_setUserAgentString.py
@@ -9,12 +9,18 @@ apks = [f for f in listdir(apk_path) if isfile(join(apk_path, f))]
 
 
 with open('spoofing_apks.txt', 'a+') as spoofing_apks_file:
-    for apk in apks:
+    for i, apk in enumerate(apks):
+        print("{}/{}".format(i, len(apks)))
         a, d, dx = AnalyzeAPK(join(apk_path, apk))
-        if not 'Landroid/webkit/WebSettings;' in dx.classes: continue
-        for meth in dx.classes['Landroid/webkit/WebSettings;'].get_methods():
-            if meth.name == 'setUserAgentString':
-                print(apk)
-                spoofing_apks_file.write("\n")
-                spoofing_apks_file.write(apk)
-                continue
+        for c in dx.get_classes():
+            #if 'Landroid/webkit/WebSettings' in c.name: continue
+            for meth in c.get_methods():
+                if meth.name == 'setUserAgentString':
+                    print("Class: {} \t APK: {}".format(c.name, apk))
+                    spoofing_apks_file.write("\n")
+                    spoofing_apks_file.write("Class: {} \t APK: {}\n".format(c.name, apk))
+                    for _, call, _ in meth.get_xref_from():
+                        print("  called by -> {} -- {}".format(call.class_name, call.name))
+                        spoofing_apks_file.write("  called by -> {} -- {}\n".format(call.class_name, call.name))
+                    spoofing_apks_file.write("\n")
+                    continue
