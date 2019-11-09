65ed1b9c6f8cfa9bfac99c7dee9c9a0536987b53
Alexander Lukjanenkov <a.lukjanenkovs@student.rug.nl>
2019-11-09 01:40:55 +0100

    Added filtering for google calls

--- find_setUserAgentString.py
+++ find_setUserAgentString.py
@@ -2,25 +2,43 @@ from androguard.misc import AnalyzeAPK
 from os import listdir
 from os.path import isfile, join
 
-
-
 apk_path = 'APKs'
 apks = [f for f in listdir(apk_path) if isfile(join(apk_path, f))]
 
+# clean files
+log = open('log.txt', 'w+')
+log.close()
+spoofing_apks_file = open('spoofing_apks.txt', 'w+')
+spoofing_apks_file.close()
 
-with open('spoofing_apks.txt', 'a+') as spoofing_apks_file:
-    for i, apk in enumerate(apks):
-        print("{}/{}".format(i, len(apks)))
-        a, d, dx = AnalyzeAPK(join(apk_path, apk))
-        for c in dx.get_classes():
-            #if 'Landroid/webkit/WebSettings' in c.name: continue
-            for meth in c.get_methods():
-                if meth.name == 'setUserAgentString':
-                    print("Class: {} \t APK: {}".format(c.name, apk))
-                    spoofing_apks_file.write("\n")
-                    spoofing_apks_file.write("Class: {} \t APK: {}\n".format(c.name, apk))
-                    for _, call, _ in meth.get_xref_from():
-                        print("  called by -> {} -- {}".format(call.class_name, call.name))
-                        spoofing_apks_file.write("  called by -> {} -- {}\n".format(call.class_name, call.name))
-                    spoofing_apks_file.write("\n")
-                    continue
+# process
+for i, apk in enumerate(apks):
+    log_strings = []
+    log_string = "{}/{}".format(i, len(apks))
+    print(log_string)
+    log_strings.append(log_string + "\n")
+
+    spoofed = False
+    a, d, dx = AnalyzeAPK(join(apk_path, apk))
+    for c in dx.get_classes():
+        for meth in c.get_methods():
+            if meth.name == 'setUserAgentString':
+                log_string = "Class: {} \t APK: {}".format(c.name, apk)
+                print(log_string)
+                log_strings.append(log_string + "\n")
+                for _, call, _ in meth.get_xref_from():
+                    log_string = "  called by -> {} -- {}".format(call.class_name, call.name)
+                    print(log_string)
+                    log_strings.append(log_string + "\n")
+                    # check if class belongs to google
+                    if not "google" in call.class_name:
+                        spoofed = True
+                continue
+    # write files
+    log = open('log.txt', 'a+')
+    log.writelines(log_strings)
+    log.close()
+    if spoofed:
+        spoofing_apks_file = open('spoofing_apks.txt', 'a+')
+        spoofing_apks_file.writelines(log_strings)
+        spoofing_apks_file.close()
